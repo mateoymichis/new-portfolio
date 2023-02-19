82b655b26c6f55aef2c08c3a831d6e12e47afa22
Mateo Fernandez <mateo.rost@gmail.com>
2023-02-19 17:30:11 -0300

    Maquetado y bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { EducationComponent } from './components/education/education.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SocialComponent } from './components/social/social.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    ProjectsComponent,
+    EducationComponent,
+    FooterComponent,
+    SocialComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
